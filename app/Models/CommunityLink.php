f44a902cb6d0403601443102e8e8e78ef0edfdce
Wachi <captainvantruc@gmail.com>
2024-09-30 18:07:22 +0200

    A7. Paginación y relaciones entre tablas

--- app/Models/CommunityLink.php
+++ app/Models/CommunityLink.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CommunityLink extends Model
-{
+{   
     use HasFactory;
+    public function creator()
+    {
+    return $this->belongsTo(User::class, 'user_id');
+    }
 }
